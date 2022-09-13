2cc00dc1a80b223025df7fe8fa586ee03c3cbd47
Nikita Fomin <fominquant@yandex.ru>
2022-09-13 16:03:48 +0300

    - Added result view and template - Added icon for home link - Updated base and home templates and styles

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 
 from flask import render_template, redirect, url_for, jsonify
@@ -13,7 +14,7 @@ def home():
     parameters_from = ParametersForm()
     if parameters_from.validate_on_submit():
         print(parameters_from.data)
-        return redirect(url_for('main.home'))
+        return redirect(url_for('main.results'))
 
     return render_template("main/home.html", parameters_from=parameters_from), 200
 
@@ -25,3 +26,10 @@ def check_json():
         data = json.load(json_form.from_json.data)
         return jsonify(data), 200
     return jsonify(json_form.errors), 400
+
+
+@main.route('/results/', methods=['GET'])
+def results():
+    now = datetime.datetime.now()
+    str_now = now.strftime("%H:%M %d.%m.%Y")
+    return render_template("main/results.html", now_date=str_now), 200
